44161eca31850b98a66b6c4d40ae2b064b6cd350
Gorz-WD <alvzrodrigo97@gmail.com>
2023-07-24 03:00:21 -0300

    add ui

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-let books = document.getElementById('books')
-let addBookForm = document.getElementById('book-form')
+const books = document.getElementById('books')
+const addBookForm = document.getElementById('book-form')
+const formContainer = document.getElementById('form-container')
+const addBtn = document.getElementById('add-btn')
+const cancel = document.getElementById('cancel')
 
 let library = []
 
-function Book(title, author, pages, read){
+function Book(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
   this.read = read
 }
 
-Book.prototype.changeReadStatus = function() {
-  this.read  === true ? this.read = false : this.read = true
+Book.prototype.changeReadStatus = function () {
+  this.read === true ? this.read = false : this.read = true
 }
 
 const addBookToLibray = (book) => library.push(book)
@@ -20,31 +23,39 @@ const displayBooks = () => {
   books.textContent = ''
   library.forEach((book, index) => {
     const card = document.createElement('div')
+    const cardDelete = document.createElement('div')
     const cardTitle = document.createElement('div')
     const cardAuthor = document.createElement('div')
     const cardPages = document.createElement('div')
     const cardReadStatus = document.createElement('div')
-    const cardDelete = document.createElement('div')
+
 
     card.classList.add('card');
     cardTitle.classList.add('card-title');
     cardAuthor.classList.add('card-author');
     cardPages.classList.add('card-pages');
     cardReadStatus.classList.add('card-read-status');
+    cardDelete.classList.add('card-delete')
 
     cardTitle.textContent = book.title
     cardAuthor.textContent = 'By ' + book.author
     cardPages.textContent = 'Pages: ' + book.pages
 
     const changeBtn = document.createElement('button')
-    changeBtn.textContent  = `${book.read === true ? 'read' : 'not read yet'}`
+    if (book.read === true) {
+      changeBtn.classList.add('read')
+    } else {
+      changeBtn.classList.add('not-read')
+    }
+    changeBtn.textContent = `${book.read === true ? 'Read' : 'Not read yet'}`
     changeBtn.addEventListener('click', () => {
       book.changeReadStatus()
       displayBooks()
     })
 
+
     const deleteBtn = document.createElement('button')
-    deleteBtn.textContent = 'delete'
+    deleteBtn.textContent = 'X'
     deleteBtn.addEventListener('click', () => {
       library.splice(index, 1)
       displayBooks()
@@ -52,28 +63,39 @@ const displayBooks = () => {
 
     cardReadStatus.appendChild(changeBtn)
     cardDelete.appendChild(deleteBtn)
+    card.appendChild(cardDelete)
     card.appendChild(cardTitle)
     card.appendChild(cardAuthor)
     card.appendChild(cardPages)
     card.appendChild(cardReadStatus)
-    card.appendChild(cardDelete)
+
     books.appendChild(card)
   });
 }
 
-addBookForm.addEventListener('submit', function(event) {
+addBookForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const pages = document.getElementById('pages').value;
   const readStatus = document.getElementById('read-status').checked;
-  
+
   const book = new Book(title, author, pages, readStatus)
-  
+
   library.push(book)
+  formContainer.style.display = 'none'
   addBookForm.reset();
   displayBooks();
 })
+
+addBtn.addEventListener('click', () => formContainer.style.display = 'flex')
+cancel.addEventListener('click', () => {
+  formContainer.style.display = 'none'
+  addBookForm.reset();
+})
+
+const book = new Book('Oathbringer', 'Brandon Sanderson', 1248, true)
+addBookToLibray(book)
 
 displayBooks()
